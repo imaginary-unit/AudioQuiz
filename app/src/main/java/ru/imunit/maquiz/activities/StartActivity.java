0bd08278b20958fa467f0d9bc17f38e230aa03b9
Constantin Petrov <petroff.cat@gmail.com>
2016-06-04 14:13:42 +1000

    Refactoring activities with fragments (in progress)

--- app/src/main/java/ru/imunit/maquiz/activities/StartActivity.java
+++ app/src/main/java/ru/imunit/maquiz/activities/StartActivity.java
@@ -1,69 +1,35 @@
 package ru.imunit.maquiz.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
 
 import ru.imunit.maquiz.R;
+import ru.imunit.maquiz.fragments.StartFragment;
 
-public class StartActivity extends Activity {
+public class StartActivity extends AppCompatActivity
+        implements StartFragment.OnFragmentInteractionListener {
 
-    Toolbar toolbar;
+    // private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-
-        initToolbar();
-
-        Button btnPlaylists = (Button)findViewById(R.id.playlists);
-        btnPlaylists.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        playlistsClick();
-                    }
-                }
-        );
     }
 
-    private void playlistsClick() {
+    @Override
+    public void onPlaylistOpen() {
         Intent playlistsIntent = new Intent(this,
                 ActivityFactory.getActivity(ActivityFactory.PLAYLISTS_VIEWER));
         startActivity(playlistsIntent);
     }
 
-    private void initToolbar() {
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.app_name);
-        toolbar.inflateMenu(R.menu.menu_main);
-        //toolbar_base.setLogo(R.drawable.playlists);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
-
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
--- app/src/main/java/ru/imunit/maquiz/fragments/StartFragment.java
+++ app/src/main/java/ru/imunit/maquiz/fragments/StartFragment.java
@@ -1,13 +1,14 @@
 package ru.imunit.maquiz.fragments;
 
-import android.app.Activity;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import ru.imunit.maquiz.R;
 
@@ -37,20 +38,34 @@ public class StartFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_start, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button bPlaylists = (Button)getView().findViewById(R.id.playlists);
+        bPlaylists.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        playlistsClick();
+                    }
+                }
+        );
+    }
+
+    private void playlistsClick() {
         if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+            mListener.onPlaylistOpen();
         }
     }
 
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        if (activity instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) activity;
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
         } else {
-            throw new RuntimeException(activity.toString()
+            throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
         }
     }
@@ -61,18 +76,7 @@ public class StartFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onPlaylistOpen();
     }
 }
